867774e286485c9d8a9d596719405b6ec0720703
Nicolas Fortin <nicolas.fortin37@gmail.com>
2020-07-11 19:32:54 +0200

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,51 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  var questions = [
+    'What\'s your favorite color?',
+    'What\'s your favorite animal?',
+  ];
+
+  void answerQuestion(int choice) {
+    print('Answer choosen $choice');
+    setState(() {
+      questionIndex = questionIndex >= questions.length - 1 ? 0 : questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello !'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () => answerQuestion(1),
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => answerQuestion(2),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => answerQuestion(3),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
